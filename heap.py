b5ed78f611b2a16f6c5f68adf96dfbdf34b20997
Michael Parker <michaelrbparker@protonmail.com>
2025-12-28 10:57:58 +0000

    heap

--- heap.py
+++ heap.py
@@ -21,17 +21,38 @@ class minheap:
         if right >= len(self.heap): return None
         return right
     
+    def bubbleup(self,nodeI):
+        pairentI = self.getparent(nodeI)
+        while ((pairentI != None) and (self.heap[pairentI] > self.heap[nodeI]) ):
+            self.heap[pairentI], self.heap[nodeI] = self.heap[nodeI], self.heap[pairentI]
+            nodeI = pairentI
+            pairentI = self.getparent(nodeI)
+
+    def bubbledown(self,nodeI):
+        leftI = self.leftchildindex(nodeI)
+        rightI = self.rightchildindex(nodeI)
+        smallestI = nodeI
+
+        if (leftI != None) and (self.heap[leftI] < self.heap[smallestI]):
+            smallestI = leftI
+        if (rightI != None) and (self.heap[rightI] < self.heap[smallestI]):
+            smallestI = rightI
+        if smallestI != nodeI:
+            self.heap[smallestI], self.heap[nodeI] = self.heap[nodeI], self.heap[smallestI]
+            self.bubbledown(smallestI)
+    
     def add(self, data):
         self.heap.append(data)
         currlen = len(self.heap) - 1
         if currlen == 0: return
-        pairentI = self.getparent(currlen)
-        while ((pairentI != None) and (self.heap[pairentI] > self.heap[currlen]) ):
-            self.heap[pairentI], self.heap[currlen] = self.heap[currlen], self.heap[pairentI]
-            currlen = pairentI
-            pairentI = self.getparent(currlen)
+        self.bubbleup(currlen)
 
-
+    def remove(self):
+        element = self.heap.pop(0)
+        self.heap.insert(0, self.heap.pop(len(self.heap)-1))
+        self.bubbledown(0)
+        return element
+      
 
 heap = minheap()
 print(heap.getmin())
@@ -47,3 +68,5 @@ print(heap.heap)
 print(heap.getmin())
 print(heap.heap[heap.leftchildindex(0)])
 print(heap.heap[heap.rightchildindex(0)])
+print(heap.remove())
+print(heap.heap)
