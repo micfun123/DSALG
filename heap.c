b87cd38a98f7d6bd651decf221ea6b419c1cf012
Michael Parker <michaelrbparker@protonmail.com>
2025-12-28 17:10:00 +0000

    learnong to use a formatter

--- heap.c
+++ heap.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h> 
-
-
+#include <stdbool.h>
 
 typedef struct minHeap
 {
@@ -11,92 +9,105 @@ typedef struct minHeap
 } minHeap;
 
 // create a minHeap instance
-minHeap* createMinHeap(int capacity)
+minHeap *createMinHeap(int capacity)
 {
-    minHeap* heap = (minHeap*)malloc(sizeof(minHeap));
-    heap->array = (int*)malloc(capacity * sizeof(int));
+    minHeap *heap = (minHeap *)malloc(sizeof(minHeap));
+    heap->array = (int *)malloc(capacity * sizeof(int));
     heap->size = 0;
     return heap;
 }
 
-bool isEmpty(minHeap* heap)
+bool isEmpty(minHeap *heap)
 {
     return heap->size == 0;
 }
 
-void swap(int* a, int* b)
+void swap(int *a, int *b)
 {
     int temp = *a;
     *a = *b;
     *b = temp;
 }
 
-
-int getparent(int nodeI){    
-    if (nodeI <= 0) return -1;
+int getparent(int nodeI)
+{
+    if (nodeI <= 0)
+        return -1;
     return (nodeI - 1) / 2;
-
 }
 
-
-int leftchildindex(int nodeI, minHeap* heap){
-    if(2 * nodeI + 1 > heap->size){return -1;}    
+int leftchildindex(int nodeI, minHeap *heap)
+{
+    if (2 * nodeI + 1 > heap->size)
+    {
+        return -1;
+    }
     return 2 * nodeI + 1;
 }
 
-
-int rightchildindex(int nodeI, minHeap* heap){
-    if(2 * nodeI + 2 > heap->size){return -1;}    
+int rightchildindex(int nodeI, minHeap *heap)
+{
+    if (2 * nodeI + 2 > heap->size)
+    {
+        return -1;
+    }
     return 2 * nodeI + 2;
 }
 
-
-void bubbleup(int nodeI, minHeap* heap){
+void bubbleup(int nodeI, minHeap *heap)
+{
     int parentI = getparent(nodeI);
-    if (parentI == -1) return;
-    while ((parentI != -1) && (heap->array[parentI] > heap->array[nodeI])){
+    if (parentI == -1)
+        return;
+    while ((parentI != -1) && (heap->array[parentI] > heap->array[nodeI]))
+    {
         swap(&heap->array[parentI], &heap->array[nodeI]);
         nodeI = parentI;
         parentI = getparent(nodeI);
     }
 }
 
-void bubbledown(int nodeI, minHeap* heap){
-    int leftI = leftchildindex(nodeI,heap);
-    int rightI = rightchildindex(nodeI,heap);
+void bubbledown(int nodeI, minHeap *heap)
+{
+    int leftI = leftchildindex(nodeI, heap);
+    int rightI = rightchildindex(nodeI, heap);
     int smallestI = nodeI;
-    if ((leftI != -1) && (heap->array[leftI] < heap->array[smallestI])){
+    if ((leftI != -1) && (heap->array[leftI] < heap->array[smallestI]))
+    {
         smallestI = leftI;
     }
-    if ((leftI != -1) && (heap->array[rightI] < heap->array[smallestI])){
+    if ((leftI != -1) && (heap->array[rightI] < heap->array[smallestI]))
+    {
         smallestI = rightI;
     }
-    if(smallestI != nodeI){
+    if (smallestI != nodeI)
+    {
         swap(&heap->array[nodeI], &heap->array[smallestI]);
-        bubbledown(smallestI,heap);
+        bubbledown(smallestI, heap);
     }
 }
 
-void add(int val,minHeap* heap){
+void add(int val, minHeap *heap)
+{
     heap->size = heap->size + 1;
     heap->array = realloc(heap->array, heap->size * sizeof(int));
-    heap->array[heap->size -1 ] = val;
-    bubbleup(heap->size - 1 ,heap);
+    heap->array[heap->size - 1] = val;
+    bubbleup(heap->size - 1, heap);
 }
 
-void pop(minHeap* heap){
+void pop(minHeap *heap)
+{
     heap->array[0] = heap->array[heap->size - 1];
     heap->size = heap->size - 1;
     heap->array = realloc(heap->array, heap->size * sizeof(int));
-    bubbledown(0,heap);
+    bubbledown(0, heap);
 }
-
 
 int main()
 {
-    
+
     minHeap *heap = createMinHeap(10);
-    
+
     printf("Initial size of the heap: %d\n", heap->size);
     printf("\n");
 
@@ -109,14 +120,16 @@ int main()
     printf("Size of the heap after adding elements: %d\n", heap->size);
 
     printf("Elements in the heap:\n");
-    for (int i = 0; i < heap->size; i++) {
+    for (int i = 0; i < heap->size; i++)
+    {
         printf("%d ", heap->array[i]);
     }
     printf("\n");
 
     pop(heap);
     printf("Elements in the heap:\n");
-    for (int i = 0; i < heap->size; i++) {
+    for (int i = 0; i < heap->size; i++)
+    {
         printf("%d ", heap->array[i]);
     }
 
